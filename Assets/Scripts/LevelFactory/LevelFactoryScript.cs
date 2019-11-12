606bb2fc726a25e8f5b5fe08742835588e319023
Dvyd <dvyd98@gmail.com>
2019-11-12 17:05:12 +0100

    ok everything is fixed now

---
+++ Assets/Scripts/LevelFactory/LevelFactoryScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class LevelFactoryScript : MonoBehaviour
+{
+    public GameObject normalTile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        readTextFile("./Assets/Scripts/LevelFactory/level1.txt");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void readTextFile(string file_path)
+    {
+        StreamReader inp_stm = new StreamReader(file_path);
+
+        string title = inp_stm.ReadLine();
+        if (title == "LEVEL1") loadLevel1(inp_stm);
+
+        //!inp_stm.EndOfStream)
+
+        inp_stm.Close();
+    }
+
+    void loadLevel1(StreamReader reader)
+    {
+        string dim = reader.ReadLine();
+        int x = int.Parse(dim.Split(' ')[0]);
+        int z = int.Parse(dim.Split(' ')[1]);
+
+        for (int j = 0; j < z; ++j)
+        {
+            string rowTiles = reader.ReadLine();
+            for (int i = 0; i < x; ++i)
+            {
+                if (rowTiles[i] == '.')
+                {
+                    GameObject obj = (GameObject)Instantiate(normalTile, new Vector3(-2 + i, 0, j), transform.rotation);
+                    obj.transform.parent = transform;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/BallMoveScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMoveScript : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        rb.velocity = transform.forward * speed;
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity += transform.right * -speed;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity += transform.right * speed;
+        }
+        //rb.AddForce(new Vector3(0.0f, 0.0f, 2) * speed);
+        //rb.AddForce(new Vector3(moveHorizontal, 0.0f, 0.0f) * speed);
+    }
+}
