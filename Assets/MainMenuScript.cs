c3094368f938dc01359ca45a620387ec37c0aa29
Dvyd <dvyd98@gmail.com>
2019-11-23 03:10:37 +0100

    ups

---
+++ Assets/MainMenuScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public bool isPlayClicked;
+    public bool isQuitClicked;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseUp()
+    {
+        if (isPlayClicked)
+        {
+            SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+        }
+        if (isQuitClicked)
+        {
+            Application.Quit();
+        }
+    }
+}
