8cf446b91e08a7686577c9350cf24e97b1b5ac16
Dvyd <dvyd98@gmail.com>
2019-12-03 14:26:15 +0100

    added game over screen and collision to obstacles

---
+++ Assets/Scripts/Obstacles/ObstacleInteractionScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ObstacleInteractionScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider otherObj)
+    {
+        if (otherObj.gameObject.CompareTag("Player"))
+
+            SceneManager.LoadScene("GameOver", LoadSceneMode.Single);
+    }
+}
