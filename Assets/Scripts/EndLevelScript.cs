dbb727b67699a95eb3b107b56f1ba6d2b6efe4d0
Dvyd <dvyd98@gmail.com>
2019-12-11 13:13:27 +0100

    added fade outs

--- Assets/Scripts/EndLevelScript.cs
+++ Assets/Scripts/EndLevelScript.cs
@@ -5,21 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class EndLevelScript : MonoBehaviour
 {
+    private bool isTriggered;
+    private int count;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isTriggered = false;
+        count = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isTriggered)
+        {
+            if (count == 0)
+            {
+                MusicPlayerScript.sound_effectPlayer.PlayOneShot(MusicPlayerScript.you_win);
+                SceneManager.LoadScene("LevelBeat", LoadSceneMode.Single);
+            }
+            --count;
+        }
     }
 
     void OnTriggerEnter(Collider otherObj)
     {
-        MusicPlayerScript.sound_effectPlayer.PlayOneShot(MusicPlayerScript.you_win);
-        SceneManager.LoadScene("LevelBeat", LoadSceneMode.Single);
+        isTriggered = true;
+        BallMoveScript.canMove = false;
+        SceneFaderScript.FadeOut();
     }
 }
